e8f7b56771f0853ba2926e2360a176a182620065
titi <74377782+qchapp@users.noreply.github.com>
2025-11-07 10:24:23 +0100

    changed sbatch script

--- src/mirage/run.sh
+++ src/mirage/run.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=med-sharded
-#SBATCH --chdir /users/$CSCS_USERNAME/datasets
-#SBATCH --output /users/$CSCS_USERNAME/reports/R-%x.%j.out
-#SBATCH --error  /users/$CSCS_USERNAME/reports/R-%x.%j.err
+#SBATCH --chdir /users/$USER/datasets
+#SBATCH --output /users/$USER/reports/R-%x.%j.out
+#SBATCH --error  /users/$USER/reports/R-%x.%j.err
 #SBATCH --nodes 32
 #SBATCH --ntasks-per-node 1
 #SBATCH --gres gpu:4
 #SBATCH --cpus-per-task 288
 #SBATCH --time 11:59:59
-#SBATCH --environment /users/$CSCS_USERNAME/.edf/sglang.toml
+#SBATCH --environment /users/$USER/.edf/sglang.toml
 #SBATCH -A a127
-
-module load cuda || true
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-export MKL_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-export PYTHONUNBUFFERED=1
 
 # --- input datasets (HF load_from_disk folders) ---
 DATA1=/capstor/store/cscs/swissai/a127/meditron/multimediset/arrow/medtrinity_conversations_1/
 DATA2=/capstor/store/cscs/swissai/a127/meditron/multimediset/arrow/medtrinity_conversations_2/
 
 # --- outputs & config ---
-OUTDIR=/users/$CSCS_USERNAME/datasets/outputs_medtrinity
+OUTDIR=/capstor/store/cscs/swissai/a127/homes/$USER/datasets/medtrinity
 CFG=config-sglang.yaml                    
-SCRIPT=rewrite_assistant_md.py            
+SCRIPT=split_script_sglang.py            
 
 mkdir -p "$OUTDIR"
 
