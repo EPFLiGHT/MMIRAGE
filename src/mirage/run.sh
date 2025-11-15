b54067bbd6036ebc86eb72b5526876de9047c08d
MichelDucartier <m.zhang2490@gmail.com>
2025-11-15 20:26:53 +0100

    Fix single node parallelization issue

--- src/mirage/run.sh
+++ src/mirage/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --chdir /users/$USER/meditron/MIRAGE/src/mirage
 #SBATCH --output /users/$USER/reports/R-%x.%j.out
 #SBATCH --error  /users/$USER/reports/R-%x.%j.err
-#SBATCH --nodes 32
+#SBATCH --nodes 2
 #SBATCH --ntasks-per-node 1
 #SBATCH --gres gpu:4
 #SBATCH --cpus-per-task 288
@@ -12,23 +12,36 @@
 #SBATCH -A a127
 
 # --- input datasets (HF load_from_disk folders) ---
-DATA1=/capstor/store/cscs/swissai/a127/meditron/multimediset/arrow/medtrinity_conversations_1/
-DATA2=/capstor/store/cscs/swissai/a127/meditron/multimediset/arrow/medtrinity_conversations_2/
+export DATA1=/capstor/store/cscs/swissai/a127/meditron/multimediset/arrow/medtrinity_conversations_1/
+export DATA2=/capstor/store/cscs/swissai/a127/meditron/multimediset/arrow/medtrinity_conversations_2/
 
 # --- outputs & config ---
-OUTDIR=/capstor/store/cscs/swissai/a127/homes/$USER/datasets/medtrinity                   
-SHARDS_ROOT="$ROOT/shards"
-MERGED_DIR="$ROOT/merged"
-CFG=/users/$USER/meditron/MIRAGE/src/mirage/config.yaml 
+export ROOT=/capstor/store/cscs/swissai/a127/homes/$USER/datasets/medtrinity                   
+export SHARDS_ROOT="$ROOT/shards"
+export MERGED_DIR="$ROOT/merged"
+export CFG=/users/$USER/meditron/MIRAGE/src/mirage/config.yaml 
+export HF_HOME=/capstor/store/cscs/swissai/a127/homes/$USER/hub
 
 mkdir -p "$SHARDS_ROOT"
 mkdir -p "$MERGED_DIR"
 
 export HF_HOME=/capstor/store/cscs/swissai/a127/homes/$USER/hf
 
-python /users/$USER/meditron/MIRAGE/src/mirage/shard_process.py \
-  --datasets "$DATA1" "$DATA2" \
-  --output_dir "$SHARDS_ROOT" \
-  --num_shards "$SLURM_JOB_NUM_NODES" \
-  --shard_id "$SLURM_NODEID" \
-  --config "$CFG"
+export CMD="python /users/$USER/meditron/MIRAGE/src/mirage/shard_process.py \
+  --datasets $DATA1 $DATA2 \
+  --output_dir $SHARDS_ROOT \
+  --num_shards $SLURM_JOB_NUM_NODES \
+  --shard_id \$SLURM_NODEID \
+  --config $CFG"
+
+echo $CMD
+
+SRUN_ARGS=" \
+  --cpus-per-task $SLURM_CPUS_PER_TASK \
+  --jobid $SLURM_JOB_ID \
+  --wait 60 \
+  -A a06 \
+  --reservation=sai-a127
+  "
+
+srun $SRUN_ARGS bash -c "$CMD"
